c09f58eb283cca5d213f9bdf8f4b11c710b746ed
RaghavPRO <raghav_ftw@hotmail.com>
2016-03-29 16:19:28 +0530

    Loading and saving data using axios and bluebird in api.

--- server.js
+++ server.js
@@ -1,9 +1,36 @@
 var express = require('express');
+var bodyParser = require('body-parser');
+var Promise = require('bluebird');
+var fs = Promise.promisifyAll(require('fs'));
+
 var app = express();
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
 var APP_DIR = __dirname + "/src/client/"
-app.get("/", function(request, respone){
+app.get("/", function(request, respone) {
   respone.sendFile(APP_DIR + "index.html");
 });
 
+app.get("/api/data", function(request, response) {
+  fs.readFileAsync("data.json").then(function(content) {
+    response.setHeader('Content-Type', 'application/json');
+    response.send(content);
+  }).catch(function(error) {
+    console.log(error);
+  });
+});
+
+app.post("/api/receive", function(request, response) {
+  fs.writeFileAsync("data.json", request.body.data).then(function(resp) {
+      response.send("Done");
+  }).catch(function(error) {
+    response.send("Error");
+  });
+});
+
+
 app.use(express.static(APP_DIR + "public"));
+
 app.listen(8080);
--- src/client/app/index.js
+++ src/client/app/index.js
@@ -2,6 +2,15 @@ import React from 'react';
 import Handsontable from 'handsontable';
 import {render} from 'react-dom';
 import { createStore } from 'redux'
+import axios from 'axios'
+
+function getDataFromServer(hot) {
+  axios.get("/api/data").then(function(response) {
+    hot.loadData(response.data);
+  }).catch(function(response) {
+    return null;
+  });
+}
 
 class SpreadSheet extends React.Component {
 
@@ -13,40 +22,46 @@ class SpreadSheet extends React.Component {
 
   componentDidMount () {
 
-  var hot = new Handsontable(document.getElementById("table"), {
-    data: [["hello", "", "", ""]],
-    minSpareRows: 1,
-    manualRowResize: false,
-  });
+    var hot = new Handsontable(document.getElementById("table"), {
+      minSpareRows: 1,
+      manualRowResize: false,
+    });
+    getDataFromServer(hot);
 
-  var data = function(state=[], action) {
-    if (action.type == "GET") {
-      return hot.getData();
+    var data = function(state=[], action) {
+      if (action.type == "GET") {
+        return hot.getData();
+      }
     }
-  }
 
-  var store = createStore(data);
-
-  store.subscribe(() =>
-    console.log(store.getState())
-  )
+    var store = createStore(data);
+    var config = {
+      headers: {'Content-Type': 'application/json'}
+    };
+    store.subscribe(() =>
+      axios.post("/api/receive", {data: JSON.stringify(store.getState())}, config).then(function(response) {
+        console.log(response);
+      }).catch(function(response) {
+        console.log(response);
+      })
+    )
 
   this.setState({store: store});
 
-  }
+}
 
-  handleClick () {
-    this.state.store.dispatch({type: 'GET'});
-  }
+handleClick () {
+  this.state.store.dispatch({type: 'GET'});
+}
 
-  render () {
-    return (
-      <div>
-        <div id="table"></div>
-        <button onClick={this.handleClick}>Dispatch</button>
-      </div>
-    );
-  }
+render () {
+  return (
+    <div>
+    <div id="table"></div>
+    <button onClick={this.handleClick}>Dispatch</button>
+    </div>
+  );
+}
 }
 
 
